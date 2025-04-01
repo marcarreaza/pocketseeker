c5cdc330fa664e2886f59952913922147645be66
marcarreaza <marc.arba02@gmail.com>
2025-04-01 18:20:55 +0200

    marc's_version

--- ConvertMol2ToPDB.py
+++ ConvertMol2ToPDB.py
@@ -1,27 +1,66 @@
 import glob
 import os
 import mdtraj as md
+from Bio import PDB
+import numpy as np
 
-for f in glob.glob("./input/*/protein.mol2"):
-    print(f"Processing {f}...")
-    
-    pdb_file = f"{os.path.splitext(f)[0]}.pdb"  # Correct path handling
 
+
+def processing(pdb_file):
+    with open(pdb_file, "r") as f:
+        lines = f.readlines()[1:]  # Remove first line
+        new_header = ["HEADER    Sample PDB file\n", "MODEL        1\n"]
+    with open(pdb_file, "w") as file:
+        file.writelines(new_header + lines)
+
+# Function to convert .mol2 to .pdb
+def convert_mol2_to_pdb(mol2_file):
+    pdb_file = f"{os.path.splitext(mol2_file)[0]}.pdb"  # Correct path handling
     try:
-        traj = md.load_mol2(f)  # Use 'f' instead of 'mol2_file'
+        traj = md.load_mol2(mol2_file)
         traj.save_pdb(pdb_file)
-        print(f"Converted {f} to {pdb_file}")
+        # Modify header only for protein.pdb
+        if "protein" in pdb_file:
+            processing(pdb_file)
+        print(f"Converted {mol2_file} to {pdb_file}")
     except Exception as e:
-        print(f"Failed to convert {f}: {e}")
+        print(f"Failed to convert {mol2_file}: {e}")
 
-for f in glob.glob("./input/*/site.mol2"):
-    print(f"Processing {f}...")
-    
-    pdb_file = f"{os.path.splitext(f)[0]}.pdb"  # Correct path handling
+# Convert all site.mol2 and protein.mol2 files
+for mol2_file in glob.glob("./input/*/*.mol2"):
+    convert_mol2_to_pdb(mol2_file)
 
+'''
+
+def compute_b_factor_from_nma(protein_pdb):
+    structure = parsePDB(protein_pdb)  # Use ProDy to parse PDB
+
+    anm = ANM("Protein NMA")
+    anm.buildHessian(structure.select("protein"))  # Select only protein atoms
+    print("Hessian built successfully.")
+    anm.calcModes(n_modes=10)
+    print("Modes calculated successfully.")
+
+    # Compute B-factors from normal modes
     try:
-        traj = md.load_mol2(f)  # Use 'f' instead of 'mol2_file'
-        traj.save_pdb(pdb_file)
-        print(f"Converted {f} to {pdb_file}")
+        bfactors = calcSqFlucts(anm[:])  # Compute from all modes
+        bfactors = (8 * np.pi**2 / 3) * bfactors  # Convert to B-factors
+        print(f"Computed bfactors for {pdb_file}")
     except Exception as e:
-        print(f"Failed to convert {f}: {e}")
+        print(f"Failed to compute bfactors {protein_pdb}: {e}")
+    # Assign computed B-factors
+    try:
+        structure.setBetas(bfactors)
+        print(f"Set for  {pdb_file}")
+    except Exception as e:
+        print(f"Failed to set {protein_pdb}: {e}")
+
+    # Save updated PDB file
+    writePDB(protein_pdb, structure)
+    print(f"Saved {protein_pdb} with computed B-factors.")
+
+for pdb_file in glob.glob("./input/*/protein.pdb"):
+    compute_b_factor_from_nma(pdb_file)
+
+'''
+
---
+++ extract_features/sasa_features.py
@@ -0,0 +1,114 @@
+import os
+import pandas as pd
+from Bio import PDB
+from Bio.PDB import DSSP, ShrakeRupley
+
+# Define residue mapping
+residue_map = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+                'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N', 
+                'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
+                'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+
+# Define atom groups
+main_chain_atoms = {"N", "CA", "C", "O"}
+polar_atoms = {"O", "N", "OH", "NH"}
+non_polar_atoms = {"C"}
+
+# Function to compute SASA
+def compute_sasa(pdb_file):
+    parser = PDB.PDBParser(QUIET=True)
+    structure = parser.get_structure("protein", pdb_file)
+    model = structure[0]
+    sa = ShrakeRupley()
+    sa.compute(model, level="A")  # Compute SASA at atom level
+    sa.compute(model, level = "R")
+    #try:
+    #    dssp = DSSP(model, pdb_file)
+    #    print(len(dssp))
+    #except Exception as e:
+    #    raise ValueError(f"DSSP failed on {pdb_file}: {e}")
+    #print(dssp.keys())  # Check if any residues exist
+
+    sasa_data = []
+    for chain in model.get_chains():
+        for residue in chain.get_residues():
+            res_name = residue.get_resname()
+            res_id = residue.get_id()[1]
+            # chain_id = chain.id
+            # dssp_key = (chain_id, res_id)
+            if res_name in residue_map:
+                #dssp_values = dssp[dssp_key]  # Extract DSSP data
+
+                # Initialize SASA values
+                total_sasa, main_sasa, side_sasa, polar_sasa, non_polar_sasa = 0, 0, 0, 0, 0
+                
+                for atom in residue.get_atoms():
+                    if atom.element == "H":  # Ignore hydrogen atoms
+                        continue
+
+                    sasa = atom.sasa
+                    total_sasa += sasa
+
+                    if atom.get_name() in main_chain_atoms:
+                        main_sasa += sasa
+                    else:
+                        side_sasa += sasa
+
+                    if atom.element in polar_atoms:
+                        polar_sasa += sasa
+                    elif atom.element in non_polar_atoms:
+                        non_polar_sasa += sasa
+
+                sasa_data.append([res_id, res_name, residue.sasa, total_sasa, main_sasa, side_sasa, polar_sasa, non_polar_sasa])
+       
+    return pd.DataFrame(sasa_data, columns=[
+        "Position", "Residue", "SASA", "Total SASA", "Main-chain SASA", "Side-chain SASA", "Polar SASA", "Non-polar SASA"
+    ])
+
+# Loop through directories and process each protein
+for folder in os.listdir('../input'):
+    folder_path = os.path.join('../input', folder)
+    if os.path.isdir(folder_path):
+        # Path to the unbound (protein) PDB file
+        protein_pdb = os.path.join(folder_path, 'protein.pdb')
+
+        if os.path.exists(protein_pdb):
+            print(f"Processing {protein_pdb}")
+
+            # Compute SASA for unbound protein
+            sasa_unbound_df = compute_sasa(protein_pdb)
+
+            # Save the SASA results to a CSV file
+            output_csv = os.path.join(folder_path, "sasa_features.csv")
+            sasa_unbound_df.to_csv(output_csv, index=False)
+            print(f"Saved results to {output_csv}")
+
+
+
+'''
+residue_data.append([
+                            res_id,  # Position
+                            res_name,  # Residue
+                            tuple(residue.center_of_mass()),  # Center of mass
+                            *(dict_physicochemical_characteristics[res_name]),  # pI, Mass, Enc, Hydrophobicty (Kyte-Doolittle scale), Polarity (Grantham scale)
+                            round(residue.sasa, 2),  # SASA_first
+                            dssp_values[2],  # Secondary Structure
+                            round(dssp_values[3], 2),  # Relative ASA
+                            dssp_values[4],  # Phi
+                            dssp_values[5],  # Psi
+                            dssp_values[6],  # NH→O_1_relidx
+                            dssp_values[7],  # NH→O_1_energy
+                            dssp_values[8],  # O→NH_1_relidx
+                            dssp_values[9],  # O→NH_1_energy
+                            dssp_values[10], # NH→O_2_relidx
+                            dssp_values[11], # NH→O_2_energy
+                            dssp_values[12], # O→NH_2_relidx
+                            dssp_values[13], # O→NH_2_energy
+                            hse_value,  # Half-sphere exposure value
+                                # The first number: the count of surrounding atoms (coordination number).
+                                # The second number: the number of atoms within the radius.
+                                # The third number: the computed HSE value (e.g., np.float64(1.0375910569929647)).
+                            hse_CA_value,
+                            hse_CB_value,
+                            hse_CN_value
+'''
