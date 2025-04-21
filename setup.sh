6f993f7ac83981231c0a1af138b5473dc0dbd7aa
Marta Pérez Villamandos <martapervil@MacBook-Air-de-Marta-3.local>
2025-04-21 17:38:31 +0200

    marta's version

--- setup.sh
+++ setup.sh
@@ -3,12 +3,34 @@
 # Nombre del entorno (puedes cambiarlo si quieres)
 ENV_NAME="pocketseeker"
 
-# Revisa si el archivo environment.yml existe
-if [ ! -f programs/environment.yml ]; then
-    echo "'environment.yml' was not found. Make sure you have it in the same directory."
+# Detectar sistema operativo
+OS_TYPE=$(uname)
+
+if [ "$OS_TYPE" == "Darwin" ]; then
+    ENV_FILE="programs/environment_mac.yml"
+elif [ "$OS_TYPE" == "Linux" ]; then
+    ENV_FILE="programs/environment_linux.yml"
+else
+    echo "Unsupported OS: $OS_TYPE"
     exit 1
 fi
 
-# Crea el entorno Conda
-echo "Creating Conda environment called '$ENV_NAME'..."
-conda env create -n $ENV_NAME -f environment.yml
+# Verificar si el archivo de entorno existe
+if [ ! -f "$ENV_FILE" ]; then
+    echo "'$ENV_FILE' was not found. Make sure it exists."
+    exit 1
+fi
+
+# Crear el entorno Conda
+echo "Creating Conda environment called '$ENV_NAME' using '$ENV_FILE'..."
+conda env create -n $ENV_NAME -f "$ENV_FILE"
+
+# Descomprimir archivos de la base de datos swissprot
+SWISSPROT_DIR="programs/swissprot"
+
+if [ -d "$SWISSPROT_DIR" ]; then
+    echo "Uncompressing files in '$SWISSPROT_DIR'..."
+    gunzip -v "$SWISSPROT_DIR"/*.gz
+else
+    echo "Directory '$SWISSPROT_DIR' not found. Skipping decompression."
+fi
